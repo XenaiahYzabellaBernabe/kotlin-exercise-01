f245a5ed3dda4f161430baf95013205e0f185135
Xenaiah Yzabella Bernabe <xenaiah.bernabe@siteminder.com>
2024-04-23 14:05:37 +0800

    Completed kotlin-exercise-01

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# eslint-jest-template
+#Complete the following exercises.
+You can use IntelliK Idea or VS Code. Which ever you prefer.
+
+1. Write a function to calculate the factorial of a given number.
+2. Write a function to check if a number is prime.
+4. Write a function to reverse a string.
+5. Write a function to find the maximum element in an array.
+6. Write a function to compute the Fibonacci sequence up to a given term.
+7. Write a function to check if a string is a palindrome.
+8. Write a function to calculate the sum of digits in a number.
+9. Write a function to find the GCD (Greatest Common Divisor) of two numbers.
+10. Write a function to count the number of vowels in a string.
+11. Write a function to convert temperature from Celsius to Fahrenheit.
+
+Create a repo called kotlin-exercise-01. 
+Commit and push your changes when your done.
---
+++ src/index.kt
@@ -0,0 +1,119 @@
+fun factorial(number: Int): Int {
+  if (number <= 1) {
+    return 1
+  }
+
+  return number * factorial(number - 1)
+}
+
+fun prime(number: Int): Boolean {
+  if (number <= 1) {
+    return false
+  } else if (number % 2 == 0) {
+    return false
+  } else {
+    return true
+  }
+}
+
+fun reverseString(text: String): String {
+  var reversed = ""
+  var index = text.length - 1
+
+  while (index >= 0) {
+    reversed += text[index]
+    index--
+  }
+
+  return reversed
+}
+
+fun maxElement(list: Array<Int>): Int {
+    var max = list[0]
+    for (number in list) {
+        if (number > max) {
+            max = number
+        }
+    }
+
+    return max
+}
+
+fun fibonacci(n: Int): List<Int> {
+    val sequence = mutableListOf(0, 1)
+
+    if (n <= 2) {
+        return sequence.take(n)
+    }
+
+    for (i in 2 until n) {
+        val nextFib = sequence[i - 1] + sequence[i - 2]
+        sequence.add(nextFib)
+    }
+
+    return sequence
+}
+
+fun palindrome(text: String): Boolean {
+  if (text == reverseString(text)) {
+    return true
+  }
+
+  return false
+}
+
+fun sumOfDigits(number: Int): Int {
+  var num = number
+  var sum = 0
+
+  while (num > 0) {
+    sum += num % 10
+    num /= 10
+  }
+
+  return sum
+}
+
+fun GCD(a: Int, b: Int): Int {
+  var num1 = a
+  var num2 = b
+
+  while (num2 != 0) {
+    val temp = num2
+    num2 = num1 % num2
+    num1 = temp
+  }
+
+  return num1
+}
+
+fun vowelCount(text: String): Int {
+  val vowels = arrayOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+  var count = 0
+
+  for (letter in text) {
+    if (letter in vowels) {
+      count++
+      }
+  }
+
+  return count
+}
+
+fun tempConverter(temp: Double): Double {
+  val farenheit = ((temp * 9 / 5) + 32)
+  return farenheit
+}
+
+fun main() {
+  println(factorial(7))
+  println(prime(10))
+  println(reverseString("Hello"))
+  println(maxElement(arrayOf(2, 3, 1, 5, 4)))
+  println(fibonacci(7))
+  println(palindrome("kayak"))
+  println(sumOfDigits(12))
+  println(GCD(8, 10))
+  println(vowelCount("Hello World Hello World"))
+  println(tempConverter((32.32)))
+}
